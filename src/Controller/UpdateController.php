6da07be24c462ff00ff6a6826f37615e84194029
Georg Wurz <wurz@cors.gmbh>
2026-03-25 09:55:41 +0100

    fix comments

--- src/Controller/UpdateController.php
+++ src/Controller/UpdateController.php
@@ -2,13 +2,17 @@
 
 declare(strict_types=1);
 
-/*
- * Austrian Standards Operations GmbH
+/**
+ * CORS GmbH.
  *
- * This software is available under the GNU General Public License version 3 (GPLv3).
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
  *
- * @copyright  Copyright (c) Austrian Standards Operations GmbH (https://www.austrian-standards.at)
- * @license    GPLv3
+ * @copyright  Copyright (c) CORS GmbH (https://www.cors.gmbh)
+ * @license    https://www.cors.gmbh/license     GPLv3 and PCL
  */
 
 namespace CORS\Bundle\WebCareBundle\Controller;
